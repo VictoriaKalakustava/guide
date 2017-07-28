410b1f950df5ae2294b716fc05fec6daac14b0d0
kolyabamberg <kolyabamberg@gmail.com>
2017-07-28 20:01:55 +0300

    added jwt authentication

--- web/src/main/java/com/itransition/guide/security/config/SecurityConfig.java
+++ web/src/main/java/com/itransition/guide/security/config/SecurityConfig.java
@@ -1,26 +1,17 @@
 package com.itransition.guide.security.config;
 
+import com.itransition.guide.security.JWTAuthenticationFilter;
 import com.itransition.guide.security.details.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import com.itransition.guide.security.JWTLoginFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -32,38 +23,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/index.html").permitAll()
-            .antMatchers("/sign-up/**").permitAll()
-            .antMatchers("/paint/**").authenticated()
-            .antMatchers("/info/**").authenticated()
-            .antMatchers("/info").authenticated()
-            .and()
-            .formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/info").failureHandler(new AuthFailureHandler())
-            .and()
-            .logout()
-            .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll()
-            .and()
-            .csrf().disable();
+        http.csrf().disable().authorizeRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers(HttpMethod.POST,"/login").permitAll()
+            .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JWTLoginFilter("/login",authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JWTAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
-    }
-
-    public PasswordEncoder getPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Component
-    public class AuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
-
-        private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-        @Override
-        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
-                throws IOException, ServletException {
-            redirectStrategy.sendRedirect(request, response, "/login");
-        }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Create a default account
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("password")
+                .roles("ADMIN");
     }
 }
